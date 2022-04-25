331a72a1ec7b75a31e50f6faf4ff18817df6e35f
itsmattpaw <itsmattpaw@gmail.com>
2022-04-25 12:47:12 -0400

    commit tree 1e52f78585d7f36b8755212f4639dae06ba75e55

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,30 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks' do 
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks/new' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect "landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id/edit' do 
+    @landmark = Landmark.find(params[:id].to_i)
+    erb :'landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id].to_i)
+    @landmark.update(params[:landmark])
+    redirect "landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id].to_i)
+    erb :'landmarks/show'
+  end
 end
